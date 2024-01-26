e736d0192dc12e86241a15289ab2a509e2dbf4f4
Zead0n <141008297+Zead0n@users.noreply.github.com>
2024-01-26 00:07:48 -0600

    Disable auto disconnect

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -18,15 +18,15 @@ use helper::*;
 use bot::Data;
 
 struct VoiceCallEvent {
-   context: serenity::Context,
-   handler: Arc<Mutex<Call>>,
+   _context: serenity::Context,
+   _handler: Arc<Mutex<Call>>,
 }
 
 impl VoiceCallEvent {
    fn new(context: serenity::Context, handler: Arc<Mutex<Call>>) -> Self {
       Self {
-         context,
-         handler
+         _context: context,
+         _handler: handler
       }
    }
 }
@@ -36,20 +36,20 @@ impl VoiceEventHandler for VoiceCallEvent {
    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
       match ctx {
          EventContext::ClientDisconnect(_) => {
-            let manager = songbird::get(&self.context)
-               .await
-               .expect("Songbird Voice client placed in at initialisation.")
-               .clone();
-            let songbird_call = self.handler.lock().await;
-            let songbird_channel_id = songbird_call.current_channel().expect("No channel id found").0;
-            let guild_channel = check_result(serenity::ChannelId::from(songbird_channel_id).to_channel(&self.context).await).guild().expect("No Guild found");
-            println!("{}", check_result(guild_channel.members(&self.context)).len());
-            let check_empty = check_result(guild_channel.members(&self.context)).len() <= 2;
-            let guild_id = guild_channel.guild_id;
+            // let manager = songbird::get(&self.context)
+            //    .await
+            //    .expect("Songbird Voice client placed in at initialisation.")
+            //    .clone();
+            // let songbird_call = self.handler.lock().await;
+            // let songbird_channel_id = songbird_call.current_channel().expect("No channel id found").0;
+            // let guild_channel = check_result(serenity::ChannelId::from(songbird_channel_id).to_channel(&self.context).await).guild().expect("No Guild found");
+            // println!("{}", check_result(guild_channel.members(&self.context)).len());
+            // let check_empty = check_result(guild_channel.members(&self.context)).len() <= 1;
+            // let guild_id = guild_channel.guild_id;
 
-            if check_empty {
-               check_result(manager.remove(guild_id).await);
-            }
+            // if check_empty {
+            //    check_result(manager.remove(guild_id).await);
+            // }
          }
          _ => {},
       }
@@ -58,7 +58,6 @@ impl VoiceEventHandler for VoiceCallEvent {
    }
 }
 
-// Return the list of commands to be registered
 pub fn get_commands() -> Vec<poise::Command<Data, StdError>> {
    vec![
       join::join(),
@@ -76,7 +75,6 @@ pub fn get_commands() -> Vec<poise::Command<Data, StdError>> {
    ]
 }
 
-// Check if handler exists and return it if it does
 pub async fn handler_exist(ctx: Context<'_>) -> Option<Arc<Mutex<Call>>> {
    let guild_id = ctx.guild_id().unwrap();
    let manager = songbird::get(ctx.serenity_context())
@@ -88,7 +86,6 @@ pub async fn handler_exist(ctx: Context<'_>) -> Option<Arc<Mutex<Call>>> {
    has_handler
 }
 
-// Join a channel and return the handler
 pub async fn join_channel(ctx: Context<'_>) -> StdResult<Arc<Mutex<Call>>> {
    let (guild_id, channel_id) = {
       let guild = ctx.guild().expect("Couldn't get guild for join_channel");
@@ -99,7 +96,7 @@ pub async fn join_channel(ctx: Context<'_>) -> StdResult<Arc<Mutex<Call>>> {
    let connect_to = match channel_id {
       Some(channel) => channel,
       None => {
-         check_message(ctx.say("Where you at?").await);
+         check_result(ctx.say("Where you at?").await);
 
          panic!("Couldn't get channel id");
       },
@@ -110,13 +107,6 @@ pub async fn join_channel(ctx: Context<'_>) -> StdResult<Arc<Mutex<Call>>> {
       .await
       .expect("Songbird Voice client placed in at initialisation.")
       .clone();
-
-   // if let Ok(handler_lock) = manager.join(guild_id, connect_to).await {
-   //    // Attach an event handler to see notifications of all track errors.
-   //    let mut handler = handler_lock.lock().await;
-   //    handler.add_global_event(TrackEvent::Error.into(), VoiceCallEvent::new(guild_id));
-   // }
-
    let handler = match manager.join(guild_id, connect_to).await {
       Ok(handler) => handler,
       Err(e) => panic!("Bruh: {:?}", e),
@@ -125,11 +115,4 @@ pub async fn join_channel(ctx: Context<'_>) -> StdResult<Arc<Mutex<Call>>> {
    handler.lock().await.add_global_event(CoreEvent::ClientDisconnect.into(), VoiceCallEvent::new(serenity_context.clone(), handler.clone()));
 
    Ok(handler)
-}
-
-// Discord check message 
-pub fn check_message(result: Result<poise::reply::ReplyHandle, poise::serenity_prelude::Error>) {
-   if let Err(e) = result {
-      panic!("Error sending check message: {:?}", e)
-   }
 }
