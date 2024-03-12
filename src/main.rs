e66f6aa635b7a63ebe00ba498e01d4c5c3c8ebfe
Zead0n <141008297+Zead0n@users.noreply.github.com>
2024-03-12 03:43:28 -0500

    Revert Error check on main
    (I'm using nvim btw)

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,11 @@ type StdResult<T> = std::result::Result<T, StdError>;
 type Context<'a> = poise::Context<'a, Data, StdError>;
 
 #[tokio::main]
-async fn main() {    
+async fn main() {
     let options = load_options();
     let mut discord_bot = check_result(load_bot(options).await);
-    
-    check_result(discord_bot.start().await);
+
+    if let Err(e) = discord_bot.start().await {
+        panic!("Discord bot failed to start (Using nvim btw): {:?}", e);
+    }
 }
