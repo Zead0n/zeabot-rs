68c544932af334c7c47bc3daa101a4a78e45e7ec
Zead0n <141008297+Zead0n@users.noreply.github.com>
2024-08-11 23:34:18 -0500

    Add queue command prototype

--- src/commands/queue.rs
+++ src/commands/queue.rs
@@ -1,28 +1,58 @@
 // use poise::serenity_prelude as serenity;
 
+use futures::future;
+use futures::StreamExt;
+
 // use crate::{Context, StdResult, commands};
+use crate::prelude::*;
+use crate::utils::*;
 
 // /// Show queue
-// #[poise::command(slash_command)]
-// pub async fn queue(
-//    ctx: Context<'_>
-// ) -> StdResult<()> {
-//    if let Some(handler) = commands::handler_exist(ctx).await {
-//       let _handler_lock = handler.lock().await;
-//       let queue_list = ctx.data().track_queue.lock().await;
-//       let mut queue_view = String::new();
+#[poise::command(slash_command)]
+pub async fn queue(ctx: Context<'_>) -> StdResult<()> {
+    let Some(player_context) = discord::get_player(&ctx) else {
+        discord::send_message(&ctx, "Not in VC").await;
+        return Ok(());
+    };
 
-//       for track in queue_list.clone().into_iter() {
-//          queue_view.push_str(format!("{}\n", track).as_str());
-//       }
-//       let reply: poise::reply::CreateReply = Default::default();
-//       let embed = serenity::CreateEmbed::new().field("Songs", queue_view, false);
+    let queue = player_context.get_queue();
+    let player = player_context.get_player().await?;
+    let max = queue.get_count().await?.min(9);
 
-//       if let Err(e) = ctx.send(poise::reply::CreateReply::embed(reply, embed)).await {
-//          panic!("failed to send queue: {:?}", e)
-//       }
+    let queue_message = queue
+        .enumerate()
+        .take_while(|(idx, _)| future::ready(*idx < max))
+        .map(|(idx, song)| match &song.track.info.uri {
+            Some(uri) => format!(
+                "**{}.** [{} - {}](<{}>)",
+                idx + 1,
+                song.track.info.title,
+                song.track.info.author,
+                uri
+            ),
+            None => format!(
+                "**{}.** {} - {}",
+                idx + 1,
+                song.track.info.title,
+                song.track.info.author
+            ),
+        })
+        .collect::<Vec<String>>()
+        .await
+        .join("\n");
 
-//    }
+    let current_message = match player.track {
+        Some(track) => match &track.info.uri {
+            Some(uri) => format!(
+                "Now playing: [{} - {}](<{}>)",
+                track.info.title, track.info.author, uri
+            ),
+            None => format!("Now playing: {} - {}", track.info.title, track.info.author),
+        },
+        None => format!("Now playing: Nothing"),
+    };
 
-//    Ok(())
-// }
+    discord::send_message(&ctx, format!("{}\n\n{}", current_message, queue_message)).await;
+
+    Ok(())
+}
